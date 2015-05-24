d98fa64ef0ec047408c855f0684b1383f3b42fed
ryan <ryan@RYPLM019-Ryan-Home.local>
2015-05-24 13:19:18 -0400

    Challenge #2

--- AgeOfLaika/ViewController.swift
+++ AgeOfLaika/ViewController.swift
@@ -25,8 +25,16 @@ class ViewController: UIViewController {
 
     @IBAction func convertAgeButton(sender: UIButton) {
         let ageInHumanYears:Int = enterAgeTextField.text.toInt()!
-        let convertedConstant:Int = 7
-        convertedAgeLabel.text = "\(ageInHumanYears * convertedConstant)" + " in Dog Years"
+        var ageInDogYears:Double
+        
+        if ageInHumanYears <= 2 {
+            ageInDogYears = Double(ageInHumanYears) * 10.5
+        }
+        else {
+            ageInDogYears = (10.5 * 2) + Double((ageInHumanYears-2) * 4)
+        }
+        
+        convertedAgeLabel.text = "\(ageInDogYears)" + " in Dog Years"
         enterAgeTextField.text = ""
         enterAgeTextField.resignFirstResponder()
         convertedAgeLabel.textColor = UIColor.blueColor()
